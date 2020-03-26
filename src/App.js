b61af835f084a091dd090f05c1f7a4187a407f22
Ian Y.E. Pan <ianpan870102@gmail.com>
2020-03-26 16:55:19 +0800

    Randomized done

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ export default class App extends React.Component {
 
   componentDidMount() {
     this.randomSeeding();
+    this.handlePlay();
   }
 
   gridClone = gridFull => {
@@ -38,10 +39,44 @@ export default class App extends React.Component {
     let newGridFull = this.gridClone(this.state.gridFull);
     for (let i = Math.floor(this.rows / 3); i < Math.floor((2 * this.rows) / 3); i++) {
       for (let j = Math.floor((2 * this.cols) / 5); j < Math.floor((3 * this.cols) / 5); j++) {
-        newGridFull[i][j] = Math.random() >= 0.75;
+        newGridFull[i][j] = Math.random() >= 0.65;
       }
     }
     this.setState({ gridFull: newGridFull });
+  };
+
+  // main function: one interation
+  play = () => {
+    let originalGrid = this.state.gridFull;
+    let newGrid = this.gridClone(this.state.gridFull);
+
+    // rules of Conway's game fo life
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        let neighbours = 0;
+
+        if (i > 0) if (originalGrid[i - 1][j]) neighbours++;
+        if (i > 0 && j > 0) if (originalGrid[i - 1][j - 1]) neighbours++;
+        if (i > 0 && j < this.cols - 1) if (originalGrid[i - 1][j + 1]) neighbours++;
+        if (j < this.cols - 1) if (originalGrid[i][j + 1]) neighbours++;
+        if (j > 0) if (originalGrid[i][j - 1]) neighbours++;
+        if (i < this.rows - 1) if (originalGrid[i + 1][j]) neighbours++;
+        if (i < this.rows - 1 && j > 0) if (originalGrid[i + 1][j - 1]) neighbours++;
+        if (i < this.rows - 1 && j < this.cols - 1) if (originalGrid[i + 1][j + 1]) neighbours++;
+
+        if (originalGrid[i][j] && (neighbours < 2 || neighbours > 3)) newGrid[i][j] = false; // dies
+        if (!originalGrid[i][j] && neighbours === 3) newGrid[i][j] = true; // lives
+      }
+    }
+    this.setState((state) => ({
+      gridFull: newGrid,
+      generation: state.generation + 1
+    }))
+  };
+
+  handlePlay = () => {
+    clearInterval(this.intervalID);
+    this.intervalID = setInterval(this.play, this.speed);
   };
 
   render() {
@@ -53,7 +88,9 @@ export default class App extends React.Component {
         <p>Generation: #{generation}</p>
         <p>
           Copyright 2020 Ian Y.E. Pan | Made with ReactJS |{' '}
-          <a target="_blank" href="https://github.com/ianpan870102/conway-game-of-life">Source Code (GitHub)</a>
+          <a target="_blank" href="https://github.com/ianpan870102/conway-game-of-life">
+            Source Code (GitHub)
+          </a>
         </p>
       </div>
     );
